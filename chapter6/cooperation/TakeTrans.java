949b12b235d8fc5ddef90b8a1f132b8896c49a8a
hak0622 <128469147+hak0622@users.noreply.github.com>
2023-03-22 17:36:22 +0900

    Add files via upload

---
+++ chapter6/cooperation/TakeTrans.java
@@ -0,0 +1,21 @@
+package cooperation;
+
+public class TakeTrans {
+
+	public static void main(String[] args) {
+
+		Student james = new Student("james", 5000);
+		Student tomas = new Student("tomas", 5000);
+		
+		Bus bus100 = new Bus(100);
+		james.takeBus(bus100);
+		james.showInfo();
+		bus100.showInfo(); //버스를 타는 경우
+		
+		Subway subwayGreen = new Subway(2);
+		tomas.takeSubway(subwayGreen);
+		tomas.showInfo();
+		subwayGreen.showInfo();  //지하철을 타는 경우
+	}
+
+}
