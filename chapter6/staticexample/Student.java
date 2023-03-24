017b4ab578bdf2a578eb3bbb4fc56686f4732ace
hak0622 <128469147+hak0622@users.noreply.github.com>
2023-03-24 15:58:11 +0900

    Add files via upload

---
+++ chapter6/staticexample/Student.java
@@ -0,0 +1,13 @@
+package staticexample;
+
+public class Student {
+
+	static int serialNum= 10000;
+	int studentID;
+	String studentName;
+	
+	public Student() {
+		serialNum++;
+		studentID = serialNum;
+	}
+}
